bacd8b2c3e4e29061d79a7dacd068afc9a7c1763
tanep117 <40617100+tanep117@users.noreply.github.com>
2018-07-23 08:32:02 +0900

    空を飛ぶインターフェースメインプログラム訂正
    空を飛ぶインターフェースメインプログラム訂正

--- chap12/sample12_2_2/SampleMain12_2_2.java
+++ chap12/sample12_2_2/SampleMain12_2_2.java
@@ -4,15 +4,26 @@ public class SampleMain12_2_2 {
 
 	public static void main(String[] args) {
 
+		/*
 		SorawoTobu[] tobumono = {
 				new AirPlane(),
 				new Helicopter(),
 				new Bird()
 		};
+		*/
 
-		for(SorawoTobu tobu: tobumono) {
-			tobu.fly();
+		AirPlane airplane1 = new AirPlane();
+		SorawoTobu airplane = new AirPlane();
+		SorawoTobu helicopter = new Helicopter();
+		SorawoTobu Bird = new Bird();
+
+		SorawoTobu[] tobu = {airplane, helicopter, Bird};
+
+
+		for(int i=0; i<tobu.length; i++) {
+			tobu[i].fly();
 		}
+
 
 	}
 
