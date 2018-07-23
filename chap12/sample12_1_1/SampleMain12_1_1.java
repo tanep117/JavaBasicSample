41b5f5141b3036de60ef2fd88afeaa0b2a445007
tanep117 <40617100+tanep117@users.noreply.github.com>
2018-07-23 11:52:59 +0900

    抽象クラスのサンプル
    抽象クラスでポリモーフィズムを使わないケースと使うケースを提示する。

--- chap12/sample12_1_1/SampleMain12_1_1.java
+++ chap12/sample12_1_1/SampleMain12_1_1.java
@@ -4,13 +4,25 @@ public class SampleMain12_1_1 {
 
 	public static void main(String[] args) {
 
-		Player[] party = { new Asobinin(),
-				new Souryo() };
+		Asobinin asobinin = new Asobinin();
+		Souryo souryo = new Souryo();
+
+		System.out.println(asobinin.getJob() + "の攻撃のターン");
+		asobinin.attack();
+		System.out.println(souryo.getJob() + "の攻撃のターン");
+		souryo.attack();
+
+
+		/*
+		 * ポリモーフィズムを使った例
+		 *
+		Player[] party = { new Asobinin(),	new Souryo() };
 
 		for (int j = 0; j < party.length; j++) {
 			System.out.println(party[j].getJob() + "の攻撃のターン");
 			party[j].attack();
 		}
+		*/
 	}
 
 }
