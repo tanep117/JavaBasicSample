2cd890fb9119bb13eea5d12cb6edbfca05f0145c
tanep117 <40617100+tanep117@users.noreply.github.com>
2022-02-09 10:05:16 +0900

    main change

--- chap14/sample14_2_2/SampMain14_2_2.java
+++ chap14/sample14_2_2/SampMain14_2_2.java
@@ -4,11 +4,15 @@ public class SampMain14_2_2 {
 
 	public static void main(String[] args) {
 		Fighter fPapasu = new Fighter("パパス");
+		Fighter fPapasu2 = new Fighter("パパス");
 		Fighter fTonnura = new Fighter("とんぬら");
 		Wizard wPapasu = new Wizard("パパス");
 
 		System.out.println("fPapasuとfPapasu");
 		System.out.println(fPapasu.equals(fPapasu));
+		System.out.println("-------------------------");
+		System.out.println("fPapasuとfPapasu2");
+		System.out.println(fPapasu.equals(fPapasu2));
 		System.out.println("-------------------------");
 		System.out.println("fPapasuとfTonnura");
 		System.out.println(fPapasu.equals(fTonnura));
